be8b44e9c4cc2b4f0ff5d68e15f04a9207980e5c
ATIII <ahtashamahsan988@gmail.com>
2022-12-10 20:53:50 +0500

    tried to upload the file (image) as input to the code using fast api

--- api/main.py
+++ api/main.py
@@ -2,20 +2,28 @@ from fastapi import FastAPI, UploadFile, File
 import uvicorn
 import numpy as np
 from io import BytesIO
+from PIL import Image
 
 app = FastAPI()
 @app.get("/ping")
 async def ping():
     return "hello i am alive"
 
-@app.post("/predict")
 
 def read_file_as_image(data)-> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
 
+
+@app.post("/predict")
 async def predict(
     file: UploadFile = File(...)
 ):
-    bytes = await file.read()
+    image = read_file_as_image(await file.read())
+    return 
+
+
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host='localhost', port=8000)
