8a27a81eb46f8d0a60bfb092392dd6fa93512120
ATIII <ahtashamahsan988@gmail.com>
2022-12-28 15:05:52 +0500

    api

--- api/main.py
+++ api/main.py
@@ -3,8 +3,13 @@ import uvicorn
 import numpy as np
 from io import BytesIO
 from PIL import Image
+import tensorflow as tf
 
 app = FastAPI()
+
+
+MODEL = tf.keras.models.load_model("../models/1")
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
 @app.get("/ping")
 async def ping():
     return "hello i am alive"
@@ -20,8 +25,14 @@ async def predict(
     file: UploadFile = File(...)
 ):
     image = read_file_as_image(await file.read())
-    return 
-
+    img_batch = np.expand_dims(image, 0)
+    predictions = MODEL.predict(img_batch)
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
 
 
 
